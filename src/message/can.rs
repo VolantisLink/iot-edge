36453a08af12e2be5f2ac971a3070f535491c895
nizq <nizq@e4hitech.com>
2022-06-20 16:16:37 +0800

    wip

--- src/message/can.rs
+++ src/message/can.rs
@@ -80,3 +80,20 @@ fn test_json() {
         println!("{}\n\n\n", s);
     }
 }
+
+#[test]
+fn test_dbc() {
+    use canparse::pgn::{PgnLibrary, SpnDefinition, ParseMessage};
+    // Parse dbc file into PgnLibrary
+    let lib = PgnLibrary::from_dbc_file("../../dbc/j1939.dbc").unwrap();
+
+    // Pull signal definition for engine speed
+    let enginespeed_def: &SpnDefinition = lib
+        .get_spn("Engine_Speed").unwrap();
+
+    // Parse frame containing engine speed
+    let msg: [u8; 8] = [0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88];
+    let engine_speed: f32 = enginespeed_def.parse_message(&msg).unwrap();
+
+    println!("Engine speed: {}", engine_speed);
+}
